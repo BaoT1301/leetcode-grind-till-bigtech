093dc0f963f4c4a763992b09d523448616afe138
Bao Tran <baot130105@gmail.com>
2026-06-04 19:46:57 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen1 = {}
+        seen2 = {}
+
+        for i in range(len(s)):
+            if s[i] in seen1:
+                seen1[s[i]] += 1
+            else:
+                seen1[s[i]] = 1
+        
+        for i in range(len(t)):
+            if t[i] in seen2:
+                seen2[t[i]] += 1
+            else:
+                seen2[t[i]] = 1
+
+        return seen1 == seen2
