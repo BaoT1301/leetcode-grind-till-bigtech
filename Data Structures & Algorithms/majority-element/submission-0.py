cc0abc4419fdcd8d7ea27f823d05f949d25742d6
Bao Tran <baot130105@gmail.com>
2026-07-16 20:12:08 -0400

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hash = {}
+
+        for num in nums:
+            if num in hash:
+                hash[num] += 1
+            else:
+                hash[num] = 1
+
+        major_element = max(hash, key=hash.get)
+
+        return major_element
