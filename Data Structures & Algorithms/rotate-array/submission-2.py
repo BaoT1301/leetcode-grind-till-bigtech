3003f166b954f48baac954dc3e43efba0e2f5351
Bao Tran <baot130105@gmail.com>
2026-09-06 18:47:22 -0400

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        res = [0] * len(nums)
+
+        for i in range(len(nums)):
+            spot = (i + k) % len(nums)
+            res[spot] = nums[i]
+
+        for i in range(len(nums)):
+            nums[i] = res[i]
+
+    
